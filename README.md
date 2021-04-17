0d2178d9e9947e8a2d0b1c4865d1a9d3496ae027
Artsiom Trubchyk <aglitchman@gmail.com>
2021-04-18 01:48:03 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ludum Dare 48 Game - Made With Defold
+
+This project was created from the "empty" project template. This is template for the future LD48. It will contain a basic setup for camera, screens, etc.
+
+## Credits
+
+Copyright (c) 2021 Artsiom Trubchyk. Licensed under the Apache License, Version 2.0. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
---
+++ ld48/src/ext.cpp
@@ -0,0 +1,39 @@
+#include <dmsdk/sdk.h>
+
+static int Dummy(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+
+    dmLogInfo("Hello, world!");
+
+    return 0;
+}
+
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] = { { "dummy", Dummy },
+                                           /* Sentinel: */
+                                           { NULL, NULL } };
+
+static void LuaInit(lua_State* L)
+{
+    int top = lua_gettop(L);
+
+    // Register lua names
+    luaL_register(L, "ld48", Module_methods);
+
+    lua_pop(L, 1);
+    assert(top == lua_gettop(L));
+}
+
+static dmExtension::Result InitializeExt(dmExtension::Params* params)
+{
+    LuaInit(params->m_L);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeExt(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+DM_DECLARE_EXTENSION(LD48, "LD48", 0, 0, InitializeExt, 0, 0, FinalizeExt)
