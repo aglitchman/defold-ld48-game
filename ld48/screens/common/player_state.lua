31dc5d206ac3689b098fb8ec3c68a265e4203ba8
Artsiom Trubchyk <aglitchman@gmail.com>
2021-04-24 22:04:59 +0300

    Update weapon anim - it's based on "yaw" now

--- ld48/screens/common/player_state.lua
+++ ld48/screens/common/player_state.lua
@@ -3,14 +3,17 @@ local M = {}
 function M.reset()
     -- Variables
     M.pos = vmath.vector3(0, 70, 0)
-    M.yaw = -90
+    M.yaw = 270
     M.pitch = 0
+    M.prev_yaw = M.yaw
+    M.prev_pitch = M.pitch
 
     M.key_state = M.key_state or {}
     M.key_state.forward = false
     M.key_state.backward = false
     M.key_state.step_left = false
     M.key_state.step_right = false
+    M.key_state.fire = false -- NOT USED
 
     -- Bobbing
     M.dist_walked = 0
@@ -22,6 +25,7 @@ function M.reset()
     M.mouse_sensitivity = 0.3
 
     -- Weapon
+    -- NOT USED YET
     M.weapon_state = M.weapon_state or {}
     M.weapon_state.energy = 99
     M.weapon_state.fire_rate = 4
