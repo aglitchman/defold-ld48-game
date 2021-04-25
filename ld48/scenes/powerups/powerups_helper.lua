d0d23e6233aa9fce158b4d384625a7fff269aad8
Artsiom Trubchyk <aglitchman@gmail.com>
2021-04-25 23:40:58 +0300

    Fix physics

--- ld48/scenes/powerups/powerups_helper.lua
+++ ld48/scenes/powerups/powerups_helper.lua
@@ -5,7 +5,7 @@ function M.spawn_blue(pos, rot)
     local obj_id = factory.create("/powerups#blue", pos, rot)
     msg.post(msg.url(nil, obj_id, "pickup_colobj"), "disable")
     go.set(msg.url(nil, obj_id, "shadow_caster"), "enabled", false)
-    go.animate(msg.url(nil, obj_id, "model"), "tint", go.PLAYBACK_LOOP_PINGPONG, vmath.vector4(1.5), go.EASING_INQUAD, 3)
+    go.animate(msg.url(nil, obj_id, "model"), "tint", go.PLAYBACK_LOOP_PINGPONG, vmath.vector4(1.5), go.EASING_INQUAD, 3, math.random() * 1.5)
     return obj_id
 end
 
